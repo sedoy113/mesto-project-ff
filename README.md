bb0ca3a192d4a53f6a2fdda4037faf0bbfb54b69
sedoy113 <sedoy113@yandex.ru>
2025-04-10 16:00:57 +0300

    add code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/sedoy113/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+const createCard = (item) => {
+	const cardTemplate = document.querySelector('#card-template').content;
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+	cardElement.querySelector('.card__image').src = item.link;
+	cardElement.querySelector('.card__title').textContent = item.name;
 
-// @todo: Функция создания карточки
+	const deleteButton = cardElement.querySelector('.card__delete-button');
+	deleteButton.addEventListener('click', () => {
+		deleteCard(cardElement);
+	});
+	return cardElement;
+}
 
-// @todo: Функция удаления карточки
+const deleteCard = (cardElement) => {
+	cardElement.remove();
+}
 
-// @todo: Вывести карточки на страницу
+const addCardsToList = (initialCards) => {
+	if (!initialCards.length) {
+		console.warn('Нет данных в массиве');
+		return;
+	}
+	const cards = initialCards.map(createCard);
+	cards.forEach(card => placesList.append(card));
+}
+
+addCardsToList(initialCards);
