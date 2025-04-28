ccf716306dc3ad74dbf80c85a7fc62fde20d77b4
sedoy113 <sedoy113@yandex.ru>
2025-04-28 15:41:46 +0300

    add del esc

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,4 +1,3 @@
-
 const popupElement = document.querySelectorAll('.popup');
 const popupClose = document.querySelectorAll('.popup__close');
 const popupTypeEdit = document.querySelector('.popup_type_edit');
@@ -7,10 +6,24 @@ const popupTypeImage = document.querySelector('.popup_type_image');
 
 const openPopup = (popup) => {
 	popup.classList.add('popup_is-opened');
+
+	const handleEscClose = (e) => {
+		if (e.key === 'Escape') {
+			closePopup(popup);
+		}
+	};
+
+	document.addEventListener('keydown', handleEscClose);
+	popup.escHandler = handleEscClose;
 };
 
 const closePopup = (popup) => {
 	popup.classList.remove('popup_is-opened');
+
+	if (popup.escHandler) {
+		document.removeEventListener('keydown', popup.escHandler);
+		delete popup.escHandler;
+	}
 };
 
 popupElement.forEach((popup) => {
