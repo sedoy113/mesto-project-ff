41278dd4aa2187491182c4f66a1bc8389d174c1e
sedoy113 <sedoy113@yandex.ru>
2025-04-30 15:03:27 +0300

    add like card

--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,7 @@ import { popupImage } from './popup.js';
 const placesList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
-const createCard = (item) => {
+const createCard = (item, likeCard) => {
 	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 	const cardImage = cardElement.querySelector('.card__image');
 	cardImage.src = item.link;
@@ -18,12 +18,18 @@ const createCard = (item) => {
 		popupImage(item);
 	});
 
+	cardElement.querySelector('.card__like-button').addEventListener('click', likeCard);
+
 	return cardElement;
+}
+
+const likeCard = (evt) => {
+	evt.target.classList.toggle('card__like-button_is-active');
 }
 
 const addCreateCard = (text, url) => {
 	const newImage = { name: text, link: url };
-	const cardElement = createCard(newImage);
+	const cardElement = createCard(newImage, likeCard);
 	placesList.prepend(cardElement);
 }
 
@@ -34,7 +40,7 @@ const addCardsToList = (initialCards) => {
 		console.warn('Нет данных в массиве');
 		return;
 	}
-	const cards = initialCards.map(createCard);
+	const cards = initialCards.map(item => createCard(item, likeCard));
 	cards.forEach(card => placesList.append(card));
 }
 
